3644dcec88bb39c30c889e14da03025a0e6b6af3
danieldjupvik <daniel@kravia.ai>
2022-09-15 13:22:37 +0200

    use external api url

--- js/settings/api.js
+++ js/settings/api.js
@@ -8,4 +8,4 @@ const hostedApiUrl = cors + "https://gamenow.herokuapp.com";
 
 export const currencyApiUrl = cors + "https://api.exchangeratesapi.io/latest";
 
-export const url = localApiUrl;
+export const url = hostedApiUrl;
