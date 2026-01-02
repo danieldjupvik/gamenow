1e907350366cd8a48486d926828c6818fa5d8c1d
danieldjupvik <daniel_22@icloud.com>
2026-01-02 17:28:39 +0100

    refactor API URLs to remove CORS proxy, using direct endpoints instead

--- js/settings/api.js
+++ js/settings/api.js
@@ -1,11 +1,8 @@
-//Cors
-const cors = "https://corsproxy.io/?";
-
 //API urls
 const localApiUrl = "http://localhost:1337";
 
-const hostedApiUrl = cors + "https://gamenow-strapi.onrender.com";
+const hostedApiUrl = "https://gamenow-strapi.onrender.com";
 
-export const currencyApiUrl = cors + "https://api.exchangeratesapi.io/latest";
+export const currencyApiUrl = "https://api.exchangeratesapi.io/latest";
 
 export const url = hostedApiUrl;
